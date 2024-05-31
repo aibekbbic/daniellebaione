a4840f70bc77cd33e84dd1c4daebedf17b633a29
aibekbbic <aibekbbic@gmail.com>
2024-05-31 11:49:11 +0200

    changed background styles

--- src/Data/sliderData.js
+++ src/Data/sliderData.js
@@ -6,7 +6,7 @@ import photo_5 from "../images/slider_image_5.jpg";
 
 const sliderData = [
   {
-    title: "Title text",
+    title: "Violino",
     text: "Poi coricarmi bisognava osservava disperata pei congiunto ero uno. Apriro divano pareva dov eri.",
     slider_image: photo_1,
   },
@@ -33,3 +33,29 @@ const sliderData = [
 ];
 
 export default sliderData;
+
+// 1.   npm install gh-pages --save-dev
+// 2.   создайте репозиторий на gh
+// 3.   git init
+// 4.   git commit -m "first commit"
+// 5.   git add .
+// 6.   git status
+// 7.   git commit -m "first commit"
+// 8.   git branch -M "main"
+// 9.   git remote add origin "ссылка"
+// 10.  git push -u origin "main"
+//      выводится:
+//      branch "main" set up to track "origin/main"
+// 11.  в packcage.json перед "name" напишите:
+//      "homepage": "https://aibekbbic.github.io/имя репозитория"
+// 12.  в packcage.json, внутри "scripts", после "start" напишите:
+//      "predeploy": "npm run build",
+//      "deploy": "gh-pages -d build",
+// 13.  npm run deploy
+//      выводятся:
+//      the build folder is ready to be deployed
+//      Published
+//      перезагружайте стр. gh с репозиторием
+// 11.  переходите settings  -> pages
+//      перезагружайте стр. gh с репозиторием
+//      выводится ссылка на репозиторий
